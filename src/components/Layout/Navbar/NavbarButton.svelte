c9def6dacf1e2ed557862c34beb2e92534970c80
NickP-real <nick_narak@rocketmail.com>
2023-07-30 15:42:13 +0700

    [WIP]: navbar

---
+++ src/components/Layout/Navbar/NavbarButton.svelte
@@ -0,0 +1,19 @@
+  <script lang="ts">
+  export let route: string;
+  export let page: string;
+  $:currentPage = page == route
+  </script>
+
+  <a
+    href={route}
+    class="@[12rem]:w-full w-12 group-hover:w-full h-12 rounded-md bg-app-blue transition-all flex items-center text-app-black px-3 gap-x-2 relative"
+    >
+    {#if currentPage}
+    <div class="absolute left-0 bg-br-app-white w-1 py-3"/>
+    {/if}
+    <span class=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-home"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg></span><p
+      class="@[12rem]:opacity-100 group-hover:opacity-100 opacity-0 transition-opacity"
+    >
+      Home
+    </p></a
+  >
---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#282c34",
+        bar: "#22262d",
+        "app-black": "#282c34",
+        "app-white": "#abb2bf",
+        "app-red": "#e06c75",
+        "app-green": "#98c379",
+        "app-yellow": "#e5c07b",
+        "app-blue": "#61afef",
+        "app-magenta": "#c678dd",
+        "app-cyan": "#56b6c2",
+        "br-app-white": "#c8cdd5",
+        "br-app-red": "#e9969d",
+        "br-app-green": "#b3d39c",
+        "br-app-yellow": "#edd4a6",
+        "br-app-blue": "#8fc6f4",
+        "br-app-magenta": "#d7a1e7",
+        "br-app-cyan": "#7bc6d0",
+        "br-app-white": "#c8cdd5",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/container-queries")],
+};
