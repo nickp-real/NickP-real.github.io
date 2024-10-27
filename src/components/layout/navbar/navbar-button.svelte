b0b29e1dcbcdd748f685b9ece9795f7c2707b9f0
nickp-real <nick_narak@rocketmail.com>
2024-10-28 02:58:38 +0700

    feat: rework of first year blog

---
+++ src/components/layout/navbar/navbar-button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  const { name, route, page }: { name: string; route: string; page: string } = $props();
+  const isCurrentPage = $derived(page == route);
+</script>
+
+<a
+  href={route}
+  class={`transition-all hover:text-app-blue ${isCurrentPage ? "text-br-app-blue font-bold" : "text-app-white"}`}
+>
+  {name}
+</a>
---
+++ src/components/layout/navbar/navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import NavbarButton from "./navbar-button.svelte";
+
+  const routes = {
+    "/": "Home",
+    "/blogs": "Blogs",
+  };
+
+  const { page }: { page: string } = $props();
+</script>
+
+<nav class="flex py-2 gap-8">
+  {#each Object.entries(routes) as [route, name]}
+    <NavbarButton {page} {route} {name} />
+  {/each}
+</nav>
